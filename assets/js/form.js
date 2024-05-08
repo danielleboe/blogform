007f8762401381eabbb8c10c0378b476c688a060
danielleboe <166262206+danielleboe@users.noreply.github.com>
2024-05-08 11:31:59 -0600

    light dark mode updates

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,6 @@
 const usernameInput = document.querySelector('#username');
 const titleInput = document.querySelector('#blogtitle');
-const contentInput = document.querySelector('#content');
+const contentInput = document.querySelector('#blogcontent');
 const submit = document.querySelector('#submit');
 const msgDiv = document.querySelector('#msg');
 
@@ -9,75 +9,28 @@ function displayMessage(type, message) {
   msgDiv.setAttribute('class', type);
 }
 
-
-
 submit.addEventListener('click', function (event) {
   event.preventDefault();
 
 const username = usernameInput.value;
 console.log('form submitted+++++++++++', username.value);
 
-const title = titleInput.value;
-const content = contentInput.value;
+const blogtitle = titleInput.value;
+const blogcontent = contentInput.value;
+
 
   if (username.trim() === '' || !username) {
     console.log('username+++++',username);
     displayMessage('error', 'Username cannot be blank');
-  } else if (title.trim() === ''  || !title) {
+  } else if (blogtitle.trim() === ''  || !blogtitle) {
     displayMessage('error', 'Title cannot be blank');
-} else if (content.trim() === '' || !content) {
+} else if (blogcontent.trim() === '' || !blogcontent) {
     displayMessage('error', 'Content cannot be blank');
   } else {
     displayMessage('success', 'Submitted successfully');
   }
 
-
-
+  localStorage.setItem('usernameInput',username);
+  localStorage.setItem('titleInput',blogtitle);
+  localStorage.setItem('contentInput',blogcontent);
 } ); 
-
-
-
-// renderLastRegistered();
-
-// function displayMessage(type, message) {
-//   msgDiv.textContent = message;
-//   msgDiv.setAttribute('class', type);
-// }
-
-// function renderLastRegistered() {
-//   const username = localStorage.getItem('username');
-//   const blogtitle = localStorage.getItem('blogtitle');
-
-
-//   if (!email || !password) {
-//     return;
-//   }
-
-//   userEmailSpan.textContent = email;
-//   userPasswordSpan.textContent = password;
-// }
-
-// signUpButton.addEventListener('click', function (event) {
-//   event.preventDefault();
-
-//   const email = emailInput.value;
-//   const password = passwordInput.value;
-
-//   if (email === '') {
-//     displayMessage('error', 'Email cannot be blank');
-//   } else if (password === '') {
-//     displayMessage('error', 'Password cannot be blank');
-//   } else {
-//     displayMessage('success', 'Registered successfully');
-
-//     localStorage.setItem('email', email);
-//     localStorage.setItem('password', password);
-//     renderLastRegistered();
-//   }
-// });
-
-// declare variables
-// capture form data (onSubmit listener) blog title, content, username
-// handle validation
-// store data to local storage as an array of objects
-// 
